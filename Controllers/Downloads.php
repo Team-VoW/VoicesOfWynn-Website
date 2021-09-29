e048c107ba3658cd41ed93e83707cc2b32b049fa
Jan Štěch <honza.stech@gmail.com>
2021-09-29 17:45:24 +0200

    Add content to the downloaded webpage

--- Controllers/Downloads.php
+++ Controllers/Downloads.php
@@ -2,6 +2,8 @@
 
 namespace VoicesOfWynn\Controllers;
 
+use VoicesOfWynn\Models\VersionManager;
+
 class Downloads extends Controller
 {
     
@@ -14,6 +16,9 @@ class Downloads extends Controller
         self::$data['base_description'] = 'Downloads of all versions of the mod are available here. Download the latest version, install it to your Minecraft client and enjoy voiced Wynncraft.';
         self::$data['base_keywords'] = 'Minecraft,Wynncraft,Mod,Voice,Downloads,Download';
     
+		$versionManager = new VersionManager();
+		self::$data['downloads_versions'] = $versionManager->getVersions();
+
         self::$views[] = 'downloads';
         return true;
     }
