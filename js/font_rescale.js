1877d6835fa568ff3eca6a54cfc414f2f0526480
Jan Štěch <honza.stech@gmail.com>
2022-02-13 20:06:53 +0100

    All the display names on the credits page should now fit containers
    The font size of every namgetag is decreased by JavaScript until it fits the parent element.

--- js/font_rescale.js
+++ js/font_rescale.js
@@ -38,3 +38,19 @@
 //         console.log('weird error');
 //     }
 // });
+
+$(function() {
+    let $nametags = $(".contributor_name"); //Get all nametags wrapper elements (<p>)
+    let nametagCount = $nametags.length;
+    for (let i = 0; i < nametagCount; i++) { //Iterate through them
+        let $currentNametag = $($nametags[i]);
+        let maxWidth = $currentNametag.width(); //Get max width that the nametag can have (should be the same for all)
+        let currentWidth = $currentNametag.find("strong").width(); //Get the width that the nametags has with the current font size
+        let currentFontSize = $currentNametag.find("strong").css('font-size').substring(0, $currentNametag.find("strong").css('font-size').length - 2); //Get the current font size (remove "px" from the returned value)
+        while (currentWidth > maxWidth) {
+            currentFontSize -= 1; //Decrease the font size by 1 px
+            $currentNametag.find("strong").css("font-size", (currentFontSize + "px")); //Apply it to the nametag
+            currentWidth = $currentNametag.find("strong").width(); //Recalculate current width, keep doing this untill the nametag fits
+        }
+    }
+});
