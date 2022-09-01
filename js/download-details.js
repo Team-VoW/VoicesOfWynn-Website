3707477da6550caf29d666c81bacc686bbbeb856
Jan Štěch <honza.stech@gmail.com>
2022-09-01 19:24:55 +0200

    Add the elements for downloading installer to download-details page
    Backend now needs implementation for the new routes

--- js/download-details.js
+++ js/download-details.js
@@ -1,7 +1,11 @@
 $("#download-btn").click(function (){
     $("#download-btn").closest("a").hide();
-    $("#download-info").hide();
     $("#download-started").show();
     $("#download-count").text((Number)($("#download-count").text()) + 1);
-    $("#report-download-issue").show();
+})
+
+$("#install-btn").click(function (){
+    $("#install-btn").closest("a").hide();
+    $("#install-started").show();
+    $("#download-count").text((Number)($("#download-count").text()) + 1);
 })
