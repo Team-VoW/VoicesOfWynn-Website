600e1fb173e6076abbf368a165c5664d7e86c12f
Jan Štěch <jan.stech@posteo.net>
2022-10-05 19:20:21 +0200

    Fix the aggregation script for daily usage stats

--- Models/Api/UsageAnalysis/PingAggregator.php
+++ Models/Api/UsageAnalysis/PingAggregator.php
@@ -8,30 +8,37 @@ use VoicesOfWynn\Models\Db;
 class PingAggregator
 {
 
-    public function aggregateDay(DateTime $latestToProcess): int
+    public function aggregateUpToDate(DateTime $firstToNotProcess): int
     {
-        $startTimestamp = $latestToProcess->getTimestamp() - 86400;
-        $startDate = (new DateTime('@'.$startTimestamp))->format('Y-m-d H:i:s');
-        $endDate = $latestToProcess->format('Y-m-d H:i:s');
-
         $db = new Db('Api/UsageAnalysis/DbInfo.ini');
-        $result = $db->fetchQuery('SELECT COUNT(*) AS "cnt" FROM ping WHERE time BETWEEN ? AND ?', array($startDate, $endDate));
-        if ($result === false) {
-            $bootups = 0;
-        }
-        else {
-            $bootups = $result['cnt'];
-        }
 
-        $db->startTransaction();
-        $result1 = $db->executeQuery('DELETE FROM ping WHERE time BETWEEN ? AND ? LIMIT ?', array($startDate, $endDate, $bootups));
-        $result2 = $db->executeQuery('INSERT INTO daily(date,bootups) VALUES (?,?)', array((new DateTime('@'.($startTimestamp + 43200)))->format('Y-m-d'), $bootups));
-        if (!($result1 && $result2)) {
-            $db->rollbackTransaction();
-            return 500;
-        }
-        $result3 = $db->commitTransaction();
+        $result = $db->fetchQuery('SELECT date FROM daily ORDER BY date DESC LIMIT 1');
+        $latestAgreggatedDate = $result['date'];
 
-        return ($result3) ? 204 : 500;
+        $currentlyProcessing = new DateTime($latestAgreggatedDate);
+        $currentlyProcessing->modify('+ 1 day');
+        while ($currentlyProcessing < $firstToNotProcess) {
+            $result = $db->fetchQuery('SELECT COUNT(*) AS "cnt" FROM ping WHERE time LIKE ?', array($currentlyProcessing->format('Y-m-d').'%'));
+            if ($result === false) {
+                $bootups = 0;
+            }
+            else {
+                $bootups = $result['cnt'];
+            }
+
+            $db->startTransaction();
+            $result1 = $db->executeQuery('DELETE FROM ping WHERE time LIKE ?', array($currentlyProcessing->format('Y-m-d').'%'));
+            $result2 = $db->executeQuery('INSERT INTO daily(date,bootups) VALUES (?,?)', array($currentlyProcessing->format('Y-m-d'), $bootups));
+            if (!($result1 && $result2)) {
+                $db->rollbackTransaction();
+                return 500;
+            }
+            if (!$db->commitTransaction()) {
+                return 500;
+            }
+
+            $currentlyProcessing->modify('+ 1 day');
+        }
+        return 204;
     }
 }
