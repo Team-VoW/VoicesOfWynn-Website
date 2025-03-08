e697ae00e6c51679006171abd742b1721f3fb11d
Jan Štěch <jan.stech@posteo.net>
2025-03-08 16:56:37 +0100

    Removed the downloads list page, removed installer from download pages

--- js/download-details.js
+++ js/download-details.js
@@ -3,10 +3,3 @@ $("#download-btn").click(function (){
     $("#download-started").show();
     $("#download-count").text((Number)($("#download-count").text()) + 1);
 })
-
-$("#install-btn").click(function (){
-    $("#install-btn").closest("a").hide();
-    $("#install-started").show();
-    $("#false-positive-info").css('border', '3px solid red');
-    $("#download-count").text((Number)($("#download-count").text()) + 1);
-})
