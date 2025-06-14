60bfd38523ae4f6215447e03ee9d0b10fdf12914
ShadyMedic <jan.stech@posteo.net>
2025-06-14 14:44:55 +0000

    Databse migration allowing NULL for some columns (for imports)
    Affected columns: table.npc_name, table.pos_x, table.pos_y, table.pos_z

---
+++ liquibase/api/changelog-2.sql
@@ -0,0 +1,13 @@
+--liquidbase formatted sql
+
+-- changeset shady:1
+
+USE api;
+
+SET NAMES utf8mb4;
+
+ALTER TABLE report
+  CHANGE npc_name npc_name VARCHAR(127) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  CHANGE `pos_x` `pos_x` MEDIUMINT NULL,
+  CHANGE `pos_y` `pos_y` MEDIUMINT NULL,
+  CHANGE `pos_z` `pos_z` MEDIUMINT NULL;
