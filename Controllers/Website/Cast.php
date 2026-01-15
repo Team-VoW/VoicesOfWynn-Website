5d4206ea8ac22ce4e37ae4734165c1040c07807d
kmaxii <esst936@gmail.com>
2026-01-15 20:30:52 +0100

    fix: enhance design consistency, improve accessibility, and refine code formatting across multiple files

--- Controllers/Website/Cast.php
+++ Controllers/Website/Cast.php
@@ -6,7 +6,7 @@ use VoicesOfWynn\Models\Website\ContentManager;
 
 class Cast extends WebpageController
 {
-	
+
 	/**
 	 * @inheritDoc
 	 */
@@ -16,26 +16,26 @@ class Cast extends WebpageController
 		$cnm = new ContentManager();
 		$voiceActor = $cnm->getVoiceActor($voiceActorId);
 		if ($voiceActor === false) {
-            //Voice actor with this ID doesn't exist
-            return 404;
-        }
+			//Voice actor with this ID doesn't exist
+			return 404;
+		}
 
 		self::$data['base_title'] = $voiceActor->getName();
 		self::$data['base_description'] = 'Do you really like a specific voice actor\'s recordings and would you like to find out more about them? Feel free to check out their personal webpage.';
 		self::$data['base_keywords'] = 'Minecraft,Wynncraft,Mod,Voice,Cast,Actor,Bio,Bios,List';
-		
+
 		self::$data['cast_voice_actor'] = $voiceActor;
 		self::$data['cast_quest_recordings'] = $cnm->getVoiceActorRecordings($voiceActorId);
-		
-        self::$data['cast_upvoted'] = $cnm->getVotes('+');
-        self::$data['cast_downvoted'] = $cnm->getVotes('-');
 
-        self::$cssFiles[] = 'cast';
-        self::$cssFiles[] = 'article-css-reset';
-        self::$cssFiles[] = 'audio-player';
+		self::$data['cast_upvoted'] = $cnm->getVotes('+');
+		self::$data['cast_downvoted'] = $cnm->getVotes('-');
+
+		self::$cssFiles[] = 'cast';
+		self::$cssFiles[] = 'article-css-reset';
+		self::$cssFiles[] = 'audio-player';
 		self::$jsFiles[] = 'voting';
 		self::$jsFiles[] = 'audio-player';
 		self::$views[] = 'cast';
-		return 200;	}
+		return 200;
+	}
 }
-
