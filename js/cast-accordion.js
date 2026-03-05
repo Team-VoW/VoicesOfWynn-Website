97d5408284ffc26927b692f8c53ecdaeea624fc2
kmaxii <esst936@gmail.com>
2026-03-05 22:52:12 +0100

    fix: fix hover on header npc to look good

--- js/cast-accordion.js
+++ js/cast-accordion.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
     $('.cast-npc-recordings').hide();
 
-    $('.cast-npc-toggle').on('click', function () {
-        const $block = $(this).closest('.cast-npc-block');
+    function toggleAccordion($header) {
+        const $block = $header.closest('.cast-npc-block');
         const $recordings = $block.find('.cast-npc-recordings');
         const isOpen = $block.hasClass('is-open');
 
         $block.toggleClass('is-open', !isOpen);
-        $(this).attr('aria-expanded', String(!isOpen));
+        $header.attr('aria-expanded', String(!isOpen));
         if (isOpen) {
             $recordings.slideUp(250);
         } else {
             $recordings.slideDown(250);
+        }
+    }
+
+    $('.cast-npc-header').on('click', function (e) {
+        if ($(e.target).closest('.cast-npc-actions').length) return;
+        toggleAccordion($(this));
+    });
+
+    $('.cast-npc-header').on('keydown', function (e) {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            toggleAccordion($(this));
         }
     });
 });
